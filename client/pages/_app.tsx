600184206af101bc9608b1bc256274a4ab66c7e8
jimmyccrypto <jimmy.c.crypto15@gmail.com>
2023-11-13 20:19:19 -0500

    fix zkevm

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,13 +4,64 @@ import { useEffect, useState } from "react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { publicProvider } from "wagmi/providers/public";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { polygonZkEvmTestnet } from "wagmi/chains";
 import Head from "next/head";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { AuthProvider } from "@components/AuthProvider";
 
+const polygonZkEvmTestnet = {
+  id: 1442,
+  name: "Polygon zkEVM Testnet",
+  network: "polygon-zkevm-testnet",
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.public.zkevm-test.net"],
+    },
+    public: {
+      http: ["https://rpc.public.zkevm-test.net"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Blockscout",
+      url: "https://explorer.public.zkevm-test.net",
+    },
+  },
+  testnet: true,
+};
+
+const scrollSepoliaTestnet = {
+  id: 534351,
+  name: "Scroll Sepolia Testnet",
+  network: "scroll-sepolia-testnet",
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://scroll-public.scroll-testnet.quiknode.pro"],
+    },
+    public: {
+      http: ["https://scroll-public.scroll-testnet.quiknode.pro"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Blockscout",
+      url: "https://sepolia-blockscout.scroll.io/",
+    },
+  },
+  testnet: true,
+};
+
 const { chains, provider } = configureChains(
-  [polygonZkEvmTestnet],
+  [polygonZkEvmTestnet, scrollSepoliaTestnet],
   [publicProvider()]
 );
 
